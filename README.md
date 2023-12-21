cba4d50c486563833e770a5b30cf892777969765
timpratim <pratim.bhosale@gmail.com>
2023-12-22 02:30:17 +0530

    readme and console input added

--- README.md
+++ README.md
@@ -1,3 +1,62 @@
-To run add credentials first to env.
-`npm install`
-`npm start`
+## Discord Reaction Collector Bot
+
+This project includes a Discord bot written in TypeScript using the `discord.js` library. 
+The bot collects unique usernames from reactions to a specific message and can randomly select a specified number of winners from these users.
+
+### Features
+1) Collects all unique usernames who reacted to a specified Discord message.
+2) Ability to select a random number of winners from the collected usernames.
+3) Saves the usernames to a text file.
+
+### Prerequisites
+- Node.js
+- TypeScript
+- discord.js library
+- A Discord Bot Token
+- message ID 
+- server ID
+- channel ID
+
+### Setup and Installation
+1) Clone the Repository
+`git clone https://github.com/timpratim/giveawaydiscordbot.git`
+
+2) Navigate to the Project Directory 
+`cd giveawaydiscordbot`
+
+3) Install Dependencies: 
+Run `npm install` to install the required Node.js modules.
+
+4) Set Environment Variables: 
+Create a .env file in the project root and add your Discord Bot Token, Channel ID, and Message ID:
+
+```
+BOT_TOKEN=your_bot_token
+CHANNEL_ID=your_channel_id
+MESSAGE_ID=your_message_id
+```
+5) Usage
+   - Enter Number of Winners: When prompted, input the number of winners 
+      you wish to select.
+   - Results: The bot will print the selected winners to the console and save all 
+      unique usernames to a file users.txt.
+
+6) Important Notes
+   - Ensure your bot has the required permissions on your Discord server.
+   - You can refer to [this](https://www.upwork.com/resources/how-to-make- 
+   discord-bot) blog.
+   - When on step 8, check the following permissions: View Channels, Read 
+     Message History, Send Messages.
+   - Never share your Discord token. Add it to your local env file. 
+   - Do not cross the rate limit for discord, else the bot will be blocked by 
+     discord. 
+   - The number of winners must be a positive integer and less than or equal to 
+     the total number of unique users.
+
+
+
+7) Run the project
+Run the project with `npm start <number of winners you want to select>
+
+8) Contributing
+Contributions to the project are welcome! Please follow the standard GitHub pull request process.
--- index.ts
+++ index.ts
@@ -1,47 +1,72 @@
 import "dotenv/config";
 import { Client, GatewayIntentBits, TextChannel } from 'discord.js';
+import fs from 'fs';
 
-const token = <string>process.env.BOT_TOKEN;
-
-const channelId = <string>process.env.CHANNEL_ID;
-
-const messageId = <string>process.env.MESSAGE_ID;
+const token = process.env.BOT_TOKEN as string;
+const channelId = process.env.CHANNEL_ID as string;
+const messageId = process.env.MESSAGE_ID as string;
 
 async function main() {
+  const winnersCount = parseInt(process.argv[2]);
+  if (isNaN(winnersCount) || winnersCount <= 0) {
+    console.error('Please provide a valid number of winners.');
+    return;
+  }
 
-  CollectionUserReactions();
-
+  const winners = await collectUserReactions(winnersCount);
+  console.log(winners);
 }
 
-main().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+async function collectUserReactions(winnersCount: number): Promise<string[]> {
+  const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-function CollectionUserReactions() {
+  return new Promise((resolve, reject) => {
+    client.once('ready', async () => {
+      try {
+        console.log(`Logged in as ${client.user?.tag}!`);
 
-  
-  const client = new Client({ intents: [GatewayIntentBits.GuildMessages, GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent] });
+        // Fetch message
+        const channel = await client.channels.fetch(channelId) as TextChannel;
+        const message = await channel.messages.fetch(messageId);
+        const uniqueUsernames = new Set<string>();
 
-  client.once('ready', async () => {
-    console.log(`Logged in as ${client.user?.tag}!`);
+        for (const reaction of message.reactions.cache.values()) {
+          const users = await reaction.users.fetch();
+          users.forEach(user => {
+            uniqueUsernames.add(user.username);
+          });
+        }
 
-    //fetch message
-    const channel = await client.channels.fetch(channelId) as TextChannel;
-    const message = await channel!.messages.fetch(messageId);
+        const usernamesArray = Array.from(uniqueUsernames);
+        const winners = selectRandomWinners(usernamesArray, winnersCount);
+        console.log('Winners:', winners);
 
-    const uniqueUsernames = new Set<string>();
+        // Write to file
+        fs.writeFile('users.txt', usernamesArray.join('\n'), (err) => {
+          if (err) {
+            console.error('Error writing to the file', err);
+            reject(err);
+            return;
+          }
+          console.log('Writting to the file');
+        });
 
-    for (const reaction of message.reactions.cache.values()) {
-      const users = await reaction.users.fetch();
-      users.forEach((user: { username: string; }) => {
-        uniqueUsernames.add(user.username);
-      });
-    }
+        client.destroy();
+        resolve(winners);
+      } catch (err) {
+        console.error(err);
+        client.destroy();
+        reject(err);
+      }
+    });
 
-    console.log(Array.from(uniqueUsernames));
-    client.destroy();
+    client.login(token);
   });
-
-  client.login(token);
 }
+
+function selectRandomWinners(usernames: string[], count: number): string[] {
+  const shuffled = [...usernames].sort(() => 0.5 - Math.random());
+  return shuffled.slice(0, count);
+}
+
+main().catch(console.error);
